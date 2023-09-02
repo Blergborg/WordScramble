370ab4d87c5b73b5281e1ca331ad18d146fc8681
Blergborg <pprater92@gmail.com>
2023-09-02 13:35:01 -0700

    Added more validation functions and used them in addNewWord() Added 3 State variables to support an error alert when the validations within addNewWord() fail and a function to make setting the State values simpler. Added .alert() modifier to the end of List component.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Game state
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    
+    // Alert state
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
     
     var body: some View {
         NavigationView {
@@ -32,6 +38,11 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame) // this is how we run code when a view comes into existence.
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
@@ -40,9 +51,29 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         // exit if the remaining string is empty
-        guard answer.count > 0 else { return }
+        guard answer.count > 0 else {
+            wordError(title: "Empty word", message: "Actually enter a word")
+            return
+        }
         
-        // more validation here
+        // exit if the user entered the prompt word
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        // exit if the word has letters not in the prompt
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        // exit if the user entered a word that's not a word in English
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
@@ -67,6 +98,44 @@ struct ContentView: View {
         
         // If we're *here* something is VERY wrong - trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    // check if the entered word is just the original prompt word
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    // Check if the user's input is a word that can be made from the random prompt word.
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    // Verify that user entry is an actual word in English
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        // NSNotFound is a special value that means the word is actually okay.
+        // we'll actually get a misspelledRange that's an NSRange
+        return misspelledRange.location == NSNotFound
+    }
+    
+    // Set alert values and show it.
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
